c0b26b4e427afc4f290fd5c0eb68f8d4cde913a2
DrinkingWater64 <aamuti64@gmail.com>
2021-04-02 23:02:28 +0600

    player collision

--- src/game.js
+++ src/game.js
@@ -8,6 +8,7 @@ let betamapData;
 let worldtilemap;
 let worldtileData;
 let tilesetSource;
+let playerAnim;
 let demoWorld;
 let player = {
 	x: 50,
@@ -30,8 +31,36 @@ let player = {
 		this.y += this.velocity;
 	},
 	show(p) {
-		p.fill(51);
-		p.rect(this.x, this.y, this.width, this.height);
+		// p.fill(51);
+		// p.rect(this.x, this.y, this.width, this.height);
+
+		if (!rar && !lar && !uar && !dar) {
+			p.image(playerAnim.srcImg, this.x, this.y, 32, 32, 0, 0, 32, 32);
+		} else if (dar) {
+			let i;
+			for (i = 0.1; i < 4; i += 1) {
+				p.image(playerAnim.srcImg, this.x, this.y, 32, 32, 32 * i, 32 * 0, 32, 32);
+			}
+			i = 0;
+		} else if (lar) {
+			let i;
+			for (i = 0; i < 4; i += 1) {
+				p.image(playerAnim.srcImg, this.x, this.y, 32, 32, 32 * i, 32 * 1, 32, 32);
+			}
+			i = 0;
+		} else if (rar) {
+			let i;
+			for (i = 0; i < 4; i += 1) {
+				p.image(playerAnim.srcImg, this.x, this.y, 32, 32, 32 * i, 32 * 2, 32, 32);
+			}
+			i = 0;
+		} else if (uar) {
+			let i;
+			for (i = 0; i < 4; i += 1) {
+				p.image(playerAnim.srcImg, this.x, this.y, 32, 32, 32 * i, 32 * 3, 32, 32);
+			}
+			i = 0;
+		}
 	},
 	update(obj) {
 		this.x += this.xspeed;
@@ -51,12 +80,45 @@ let player = {
 		}
 	},
 };
+
+let viewport = {
+	screen: [0, 0],
+	startTile: [0, 0],
+	endTile: [0, 0],
+	offset: [0, 0],
+	update(px, py) {
+		this.offset[0] = Math.floor(this.screen[0] / 2 - px); // 32 is tile width
+		this.offset[1] = Math.floor(this.screen[1] / 2 - py);
+		let tile = [Math.floor(px / 32), Math.floor(py / 32)]; // 32 is tile height and width
+		this.startTile[0] = tile[0] - 1 - Math.ceil(this.screen[0] / 2 / 32);
+		this.startTile[1] = tile[1] - 1 - Math.ceil(this.screen[1] / 2 / 32);
+
+		if (this.startTile[0] < 0) {
+			this.startTile[0] = 0;
+		}
+		if (this.startTile[1] < 0) {
+			this.startTile[1] = 0;
+		}
+		this.endTile[0] = tile[0] + 1 + Math.ceil(this.screen[0] / 2 / 32);
+		this.endTile[1] = tile[1] + 1 + Math.ceil(this.screen[1] / 2 / 32);
+
+		if (this.endTile[0] >= 1280) {
+			this.endTile[0] = 1280 - 1; // 1280 is width of map
+		}
+		if (this.endTile[1] >= 640) {
+			this.endTile[1] = 640 - 1;
+		}
+	},
+};
+
 const sketch = (p) => {
 	p.preload = () => {
 		betamapData = p.loadJSON('../maps/betamap.json');
 		//		worldtileData = p.loadJSON('../maps/newmap.json');
 		betamapData.worldtilemap = p.loadImage('../images/worldtileset.png');
 		tilesetSource = p.loadJSON('../maps/worldtileset.json');
+		playerAnim = p.loadJSON('../maps/playeranim.json');
+		playerAnim.srcImg = p.loadImage('../images/player.png');
 	};
 	setInterval(console.log('5 seconds'), 5000);
 	p.setup = () => {
@@ -65,6 +127,7 @@ const sketch = (p) => {
 		// console.log(worldtileData.layers[0].data.length);
 		p.frameRate(15);
 		demoWorld = WorldRenderer(betamapData, tilesetSource, 32, 32, player);
+		viewport.screen = [1280, 640];
 	};
 
 	p.draw = () => {
@@ -95,6 +158,9 @@ const sketch = (p) => {
 			uar = false;
 			dar = false;
 		}
+		viewport.update(player.x + player.width / 2, player.y + player.height / 2);
+		//	p.fill(0);
+		//	p.rect(0, 0, viewport.screen[0], viewport.screen[1]);
 	};
 };
 
